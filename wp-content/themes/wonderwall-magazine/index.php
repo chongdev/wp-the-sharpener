b27a173eadba3c1d2d741a60c9c3210acf25da46
Chong <monkey.d.chong@gmail.com>
2020-05-02 17:56:30 +0700

    Update index.php

--- wp-content/themes/wonderwall-magazine/index.php
+++ wp-content/themes/wonderwall-magazine/index.php
@@ -34,7 +34,7 @@ $max_columns = 12 / $post_columns;
 
 									// thumbnail sizes
 									$thumb_width = 240;
-									$thumb_height = $thumb_width / 0.81;
+									$thumb_height = 240; //$thumb_width / 0.81;
 									$mobile_thumb_height = 480 / 1.56;
 
 									// column class
